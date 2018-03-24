ae62ebdb68eb211580f6dbb13d7e76e0a0be5f18
SML <dltkdals2202@gmail.com>
2018-03-25 00:06:32 +0900

    로그인 기능 구축완료

--- routes/login/signin.js
+++ routes/login/signin.js
@@ -2,26 +2,32 @@ var express = require('express');
 var router = express.Router();
 var UserData = require('../../config/dbconfig');
 var session = require('express-session');
-router.post('/', function(req,res,next){
-  UserData.find({email:req.body.email,password:req.body.password}, function(err, data) {
+router.post('/', function(req, res, next) {
+  UserData.find({
+    email: req.body.email,
+    password: req.body.password
+  }, function(err, data) {
     if (err) {
       res.status(500).send({
-        stat : "fail",
+        stat: "fail",
       });
       console.log("find error");
     } else {
-      if(data[0].email){
+      if (data[0]) {
+        let email = data[0].email;
+        req.session.email = email;
+        console.log(req.session.email);
+        req.session.save();
         res.status(201).send({
-          stat : "success",
-          message : "login success"
+          stat: "success",
+          message: "login success"
         });
       }else{
-        res.status(201).send({
-          stat : "fail",
-          message : "login fail"
+        res.status(500).send({
+          stat: "fail",
+          message: "login fail"
         });
       }
-
     }
   });
 });
